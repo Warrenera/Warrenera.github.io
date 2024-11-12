38d5cf77bee0bc3ef82abac9c59216693bb451d0
Andrew <15279886+Warrenera@users.noreply.github.com>
2024-11-11 19:55:22 -0500

    Removed commented practice JS

--- cannections.js
+++ cannections.js
@@ -1,15 +1,3 @@
-/* Changes background color to a random color with every button press. For practice purposes
-function random(number) {
-	return Math.floor(Math.random() * (number + 1));
-}
-
-document.querySelectorAll('.square').forEach(function(button) {
-	button.addEventListener('click', () => {
-		const randomColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
-		document.body.style.backgroundColor = randomColor;
-	});
-});*/
-
 let selectedCount = 0;
 
 document.querySelectorAll('.square').forEach(button => {
