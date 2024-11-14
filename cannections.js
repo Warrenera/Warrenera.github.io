a66983cafb320e7dea72b7d90b5f0391de4568f0
Andrew Warrener <15279886+Warrenera@users.noreply.github.com>
2024-11-13 23:25:04 -0500

    Added rest of JSON, first pass at logic to populate buttons

--- cannections.js
+++ cannections.js
@@ -1,3 +1,5 @@
+const categorySize = 4;
+
 async function getData() {	
 	const url = 'https://warrenera.github.io/cannections.json';
 	const request = new Request(url);
@@ -5,26 +7,51 @@ async function getData() {
 	return await response.json();
 }
 
-const categories = getData();
+function getNumbers(length) {
+	// Generates unique random numbers
+	const numbers = new Set();
+	while(numbers.size !== categorySize) {
+		numbers.add(Math.floor(Math.random() * length + 1));
+	}
+	return numbers;
+}
 
-let selectedCount = 0;
-
-document.querySelectorAll('.square').forEach(button => {
-	button.textContent = categories[0]["topics"][0];
-	
-	let selected = false;
-	const classes = button.classList;
-	
-	button.addEventListener('click', () => {
-		if (selected) {
-			selected = false;
-			selectedCount--;
-			classes.toggle('selected');
-		} else if (selectedCount < 4) {
-			selected = true;
-			selectedCount++;
-			classes.toggle('selected');
+function getCategories(categories, numbers) {
+	for (let i = 0; i < categorySize; i++) {
+		for (const n of numbers) {
+			chosenCategories[i] = categories[n];			
 		}
-		console.log('Buttons selected: ' + selectedCount);
+	}
+}
+
+async function main() {	
+	const data = await getData();
+	const numbers = getNumbers(data.length);
+	const categories = getCategories(data, numbers);
+	console.log(categories);
+	
+	let selectedCount = 0;
+	
+	document.querySelectorAll('.square').forEach((button, i) => {
+		button.textContent = categories[0]['topics'][0];
+		
+		let selected = false;
+		const classes = button.classList;
+		
+		button.addEventListener('click', () => {
+			if (selected) {
+				selected = false;
+				selectedCount--;
+				classes.toggle('selected');
+			} else if (selectedCount < 4) {
+				selected = true;
+				selectedCount++;
+				classes.toggle('selected');
+			}
+			console.log('Buttons selected: ' + selectedCount);
+		});
 	});
-});
+
+}
+
+main();
