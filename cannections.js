48a260445882be5b88df8652fcde4ee05c626335
Andrew Warrener <15279886+Warrenera@users.noreply.github.com>
2024-11-13 21:30:07 -0500

    Added JSON, testing sampling values from it

--- cannections.js
+++ cannections.js
@@ -1,7 +1,16 @@
+async function getData() {	
+	const url = 'https://warrenera.github.io/cannections.json';
+	const request = new Request(url);
+	const response = await fetch(request);
+	return await response.json();
+}
+
+const categories = getData();
+
 let selectedCount = 0;
 
 document.querySelectorAll('.square').forEach(button => {
-	button.textContent = 'Test';
+	button.textContent = categories[0]["topics"][0];
 	
 	let selected = false;
 	const classes = button.classList;
