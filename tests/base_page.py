0da011770c3a7f1ca90f281f383e0c87217c6bdd
Andrew <15279886+Warrenera@users.noreply.github.com>
2026-09-24 20:13:39 -0400

    Added create_wait for custom waits

--- tests/base_page.py
+++ tests/base_page.py
@@ -26,39 +26,61 @@ class BasePage:
         """Set the details for interacting with the page."""
         self.driver = driver
         self.timeout = timeout
-        self._wait = WebDriverWait(self.driver, self.timeout)
+        self._wait = self.create_wait(timeout)
 
-    def get_background_color(self, element: WebElement) -> str:
+    def create_wait(self, timeout: int | None = None) -> WebDriverWait:
+        """Make a wait timer for waiting on expected conditions.
+
+        For most uses the default self._wait will suffice; however, if a
+        different timeout length is desired a new wait WebDriverWait
+        needs to be created. `timeout` is the number of seconds to wait.
+        """
+        return WebDriverWait(self.driver, timeout or self.timeout)
+
+    def get_background_color(self, element: WebElement, wait: WebDriverWait | None = None) -> str:
         """Get an element's background-color CSS property hex code."""
-        return self._wait.until(
+        waiter = wait or self._wait
+        return waiter.until(
             lambda _: Color.from_string(element.value_of_css_property("background-color")).hex,
             message=f"Unable to get the background-color of element {element}",
         )
 
-    def click(self, locator: tuple[ByType, str] | WebElement) -> None:
+    def click(
+        self,
+        locator: tuple[ByType, str] | WebElement,
+        wait: WebDriverWait | None = None,
+    ) -> None:
         """Click the specified UI button."""
-        self._wait.until(
+        waiter = wait or self._wait
+        waiter.until(
             element_to_be_clickable(locator),
             message=f"Unable to click the element with locator {locator}",
         ).click()
 
-    def do_not_find(self, locator: tuple[ByType, str]) -> bool:
+    def do_not_find(self, locator: tuple[ByType, str], wait: WebDriverWait | None = None) -> bool:
         """Verify element expected to be invisible is invisible."""
-        return self._wait.until(
+        waiter = wait or self._wait
+        return waiter.until(
             invisibility_of_element_located(locator),
             message=f"Didn't expect to find element with locator {locator}, but found it anyway",
         )
 
-    def find(self, locator: tuple[ByType, str]) -> WebElement:
+    def find(self, locator: tuple[ByType, str], wait: WebDriverWait | None = None) -> WebElement:
         """Search for the specified UI element and return if found."""
-        return self._wait.until(
+        waiter = wait or self._wait
+        return waiter.until(
             message=f"element with locator {locator} not found!",
             method=visibility_of_element_located(locator),
         )
 
-    def find_all(self, locator: tuple[ByType, str]) -> list[WebElement]:
+    def find_all(
+        self,
+        locator: tuple[ByType, str],
+        wait: WebDriverWait | None = None,
+    ) -> list[WebElement]:
         """Search for all UI elements and return if found."""
-        return self._wait.until(
+        waiter = wait or self._wait
+        return waiter.until(
             visibility_of_all_elements_located(locator),
             message=f"Unable to get all elements with locator {locator}",
         )
